338bca92d5657eb23ae8ae9759b2d4e652f073d8
lutfiqaraman <lutfiqaraman@gmail.com>
2020-07-31 19:11:16 +0300

    add dotenv to the project

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
-
 const express = require('express');
 const app = express();
+
+require('dotenv').config();
+
+const port = process.env.PORT;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World ...');
 });
 
-app.listen(3000);
+app.listen(port);
