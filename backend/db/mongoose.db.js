e2dd28199c5386391643dfb32100df64fd8b82ff
lutfiqaraman <lutfiqaraman@gmail.com>
2020-08-01 18:31:30 +0300

    connect to the mongo database

--- backend/db/mongoose.db.js
+++ backend/db/mongoose.db.js
@@ -1,2 +1,23 @@
 const mongoose = require('mongoose');
+require('dotenv').config({ path: "../.env" });
 
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const database   = process.env.DB_NAME;
+
+const url = `mongodb+srv://${username}:${password}@cluster0-eqore.mongodb.net/`
+
+const options = {
+  dbName: database,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+mongoose
+  .connect(url, options)
+  .then(() => {
+    console.log('connect to mongo cloud');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
