b137677502f834e6da7555a5ad6e9d2c1d221ccd
lutfiqaraman <lutfiqaraman@gmail.com>
2020-08-07 11:11:41 +0300

    add link to events page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-
+import { EventComponent } from './components/event/event.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'events', component: EventComponent}
 ];
 
 @NgModule({
