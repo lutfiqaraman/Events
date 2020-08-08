25ba69e180c56fe253a80088ae041e09bad659c7
lutfiqaraman <lutfiqaraman@gmail.com>
2020-08-08 18:28:22 +0300

    create an event model

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -23,7 +24,8 @@ FullCalendarModule.registerPlugins([
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FullCalendarModule
+    FullCalendarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/models/event.model.ts
@@ -0,0 +1,8 @@
+export interface IEvent {
+  id?: string;
+  title: string;
+  description: string;
+  className: string;
+  start: Date;
+  end: Date;
+}
