143b7a83edd772d9fcb57c40a68d51c102e203f2
lutfiqaraman <lutfiqaraman@gmail.com>
2022-07-21 22:27:08 +0300

    build calendar app

--- frontend/src/app/components/event/event.component.ts
+++ frontend/src/app/components/event/event.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-event',
@@ -7,11 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event.component.css']
 })
 export class EventComponent implements OnInit {
+  eventModal: Modal | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
 
+  }
+
+  open() {
+    this.eventModal = new Modal(document.getElementById('eventModal'), {
+      keyboard: false
+    });
+    this.eventModal.show();
   }
 
 }
